c56d96f99c01533d637cf39c9d0367fc637bcd2f
Kacper Tywonek <166203364+Typ3kq@users.noreply.github.com>
2024-04-06 09:24:39 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Point point = new Point(3.5, 2.0);
+
+        
+        System.out.println("Współrzędne punktu:");
+        System.out.println("x = " + point.getX());
+        System.out.println("y = " + point.getY());
+    }
+}
