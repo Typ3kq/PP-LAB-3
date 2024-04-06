9a9708f4f478b1de62a432b96a365277c5f4cde6
Kacper Tywonek <166203364+Typ3kq@users.noreply.github.com>
2024-04-06 09:47:47 +0200

    zadani3

---
+++ Main.java
@@ -0,0 +1,19 @@
+import geometry.ColoredCircle;
+import geometry.Point;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Point center = new Point(0, 0);
+
+
+        ColoredCircle coloredCircle = new ColoredCircle(center, 5.0, "blue");
+
+
+        System.out.println("Właściwości kolorowego koła:");
+        System.out.println("Środek koła: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
+        System.out.println("Promień koła: " + coloredCircle.getRadius());
+        System.out.println("Kolor koła: " + coloredCircle.getColor());
+        System.out.println("Obwód koła: " + coloredCircle.calculatePerimeter());
+    }
+}
