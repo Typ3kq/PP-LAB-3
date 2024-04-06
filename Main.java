3e8d905b60b806bbaee2a581720624e975ab61e1
Kacper Tywonek <166203364+Typ3kq@users.noreply.github.com>
2024-04-06 09:42:17 +0200

    zadanie2

---
+++ Main.java
@@ -0,0 +1,18 @@
+import geometry.Circle;
+import geometry.Point;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Point center = new Point(0, 0);
+
+
+        Circle circle = new Circle(center, 5.0);
+
+
+        System.out.println("Właściwości koła:");
+        System.out.println("Środek koła: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
+        System.out.println("Promień koła: " + circle.getRadius());
+        System.out.println("Obwód koła: " + circle.calculatePerimeter());
+    }
+}
